f0264a736c00cd5f277aabe640519fadef40582c
ovidio ServerMac <ovidiolopezt@gmail.com>
2017-06-06 17:01:19 -0500

    Areas

---
+++ OMI/Controllers/AreasController.cs
@@ -0,0 +1,20 @@
+﻿using OMI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OMI.Controllers
+{
+    public class AreasController : Controller
+    {
+        // GET: Areas
+        public ActionResult Index()
+        {
+            UtilApp util = new UtilApp();
+
+            return View(util.GetAreas());
+        }
+    }
+}
---
+++ OMI/Models/UtilApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OMI.Models
+{
+    public class UtilApp
+    {
+        OPEntities entidad;
+        public UtilApp()
+        {
+            entidad = new OPEntities();
+        }
+
+        public List<TbArea> GetAreas()
+        {
+
+            return entidad.TbArea.ToList();
+        }
+
+    }
+
+}
