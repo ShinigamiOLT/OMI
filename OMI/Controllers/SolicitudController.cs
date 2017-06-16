c2a2a259dad9f9a1b801df20b23c0341c096f7f8
ovidio ServerMac <ovidiolopezt@gmail.com>
2017-06-15 19:10:37 -0500

    xxxx
    # Conflicts:
    #	OMI/Controllers/SolicitudController.cs
    #	OMI/OMI.csproj
    #	OMI/Views/Solicitud/AllSolicitud.cshtml

--- OMI/Controllers/SolicitudController.cs
+++ OMI/Controllers/SolicitudController.cs
@@ -300,5 +300,30 @@ namespace OMI.Controllers
         {
             return RedirectToAction("AllSolicitud", "Solicitud", new { id });
         }
+<<<<<<< HEAD
+=======
+
+        public ActionResult Autorizar()//string parent, int p1, string a, string b)
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Autorizar(AutorizaInput entrada)
+        {
+            return Json(entrada);
+        }
+        [HttpGet]
+        public JsonResult Eliminar(int id = 0)
+        {
+
+           // int idsol = (int)Session["IdSolicitud"];
+            cSolicitud sol = new cSolicitud(id,3);
+            sol.contexto.TbSolicitud.Find(id).Enviado = 1;
+            sol.contexto.SaveChanges();
+        
+            return Json(id, JsonRequestBehavior.AllowGet);
+        }
+
+>>>>>>> 0c5bca2... xxxx
     }
 }
---
+++ OMI/Models/FrontUser.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace OMI.Models
+{
+    public class FrontUser
+    {
+        /*   public static bool TienePermiso(RolesPermisos valor)
+           {
+               var usuario = FrontUser.Get();
+               return !usuario.Rol.Permiso.Where(x => x.PermisoID == valor)
+                   .Any();
+           }
+   
+           public static Usuario Get()
+           {
+               return new Usuario().Obtener(SessionHelper.GetUser());
+           }
+           * /
+   
+           
+       }
+   
+       public class SessionHelper
+       {
+       }
+   
+       public class Usuario
+       {
+          
+           
+       }
+   
+       public enum RolesPermisos
+       {
+           #region Alumnos
+           Alumno_Puede_Crear_Nuevo_Registro = 2,
+           Alumno_Puede_Eliminar_Registro = 3,
+           Alumno_Puede_Visualizar_Un_Alumno = 4,
+           #endregion
+       }
+       public class PermisoAttribute : ActionFilterAttribute
+       {
+           public RolesPermisos Permiso { get; set; }
+   
+           public override void OnActionExecuting(ActionExecutingContext filterContext)
+           {
+               base.OnActionExecuting(filterContext);
+   
+               if (!FrontUser.TienePermiso(this.Permiso))
+               {
+                   filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+                   {
+                       controller = "Home",
+                       action = "Index"
+                   }));
+               }
+           }
+      */
+    }
+}
